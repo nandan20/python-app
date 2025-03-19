1255923bd1a61be12736c1b28200311d25267b7c
ninandan <ninandan@cisco.com>
2025-03-19 11:59:53 +0530

    dry run

--- app/test_app.py
+++ app/test_app.py
@@ -4,4 +4,4 @@ from app.server import app
 def test_home():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
+    assert response.data == b"Hello, World!" 
